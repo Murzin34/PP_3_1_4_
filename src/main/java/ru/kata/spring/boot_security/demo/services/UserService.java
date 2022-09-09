32d12c339234a27ca7742b1c45d8d16fc0ab2cf2
Murzin34 <murzinvolg@yandex.ru>
2022-09-09 10:56:30 +0300

    ver 8

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -6,7 +6,7 @@ import ru.kata.spring.boot_security.demo.models.User;
 import java.util.List;
 
 
-public interface UserService{
+public interface UserService {
 
     List<User> getAllUsers();
     User getById(int id);
